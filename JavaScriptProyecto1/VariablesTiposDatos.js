bccc0db85a4f09e291e9010d62786da35c53551c
LizethVictoria20 <lizvictoria754@gmail.com>
2024-04-13 15:02:51 -0500

    Funcion que calcula el indice de masa corporal

--- JavaScriptProyecto1/VariablesTiposDatos.js
+++ JavaScriptProyecto1/VariablesTiposDatos.js
@@ -7,15 +7,28 @@ function Convertion(){
   console.log(`${celcius}ºC son ${fahrenheit} grados en Fahrenheit`);
 }
 
+function IndiceMasaCorporal(){
+  console.log("Calcula el Indice de Masa Corporal");
+  let kilogramos = prompt("Por favor, ingresa tu peso en kilogramos: ");
+  let altura = prompt("Por favor, ingresa tu altura en metros: ");
+  let TotalCalculoMasaCorporal = kilogramos /( altura ** 2);
+  let DecimalesCalculoMasaCorporal = TotalCalculoMasaCorporal.toFixed(2)
+  console.log(`Tu indice de masa corporal (IMC) es: ${DecimalesCalculoMasaCorporal}`)
+}
+
+
 function Menu(){
   console.log("\nMenú:");
   console.log("1. Convertir Celsius a Fahrenheit ");
-  console.log("2. Calcular IMC ");
-  let option = prompt("Seleccione una opción: ")
-  console.log(typeof(option))
+  console.log("2. Calcular Indice de Masa Corporal (IMC) ");
+  let option = prompt("Seleccione una opción: ");
+
   switch(option) {
     case "1":
-      Convertion()
+      Convertion();
+      break;
+    case "2":
+      IndiceMasaCorporal()
   }
 }
 
