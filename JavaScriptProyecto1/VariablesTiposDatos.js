2fae431a78a2229059ab30714d747d68d7163552
LizethVictoria20 <lizvictoria754@gmail.com>
2024-04-13 18:46:46 -0500

    Agregando funcion de generación de usuario

--- JavaScriptProyecto1/VariablesTiposDatos.js
+++ JavaScriptProyecto1/VariablesTiposDatos.js
@@ -16,19 +16,32 @@ function IndiceMasaCorporal(){
   console.log(`Tu indice de masa corporal (IMC) es: ${DecimalesCalculoMasaCorporal}`)
 }
 
+function GeneradorNombreUsuario(){
+  console.log ("Generardor nombre de usuario");
+  let nombre = prompt("Nombre: ");
+  let apellido = prompt("Apellido: ");
+  let AnioNacimiento = prompt("Año de nacimiento: ");
+  let NombreUsuario = nombre + apellido + AnioNacimiento ;
+  console.log(`Nombre de usuario combinado: ${NombreUsuario}`);
+}
 
 function Menu(){
   console.log("\nMenú:");
   console.log("1. Convertir Celsius a Fahrenheit ");
   console.log("2. Calcular Indice de Masa Corporal (IMC) ");
+  console.log("3. Generar nombre de usuario");
   let option = prompt("Seleccione una opción: ");
 
   switch(option) {
     case "1":
       Convertion();
-      break;
+      Menu();
     case "2":
-      IndiceMasaCorporal()
+      IndiceMasaCorporal();
+      Menu();
+    case "3":
+      GeneradorNombreUsuario();
+      Menu();
   }
 }
 
