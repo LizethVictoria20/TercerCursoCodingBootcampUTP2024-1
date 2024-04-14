8e4ed38ebb2239d97fa2292662d74419f74c9de7
LizethVictoria20 <lizvictoria754@gmail.com>
2024-04-14 13:15:04 -0500

    Agregando funcion de contador de vocales de una frase

--- JavaScriptProyecto1/EstructurasControl.js
+++ JavaScriptProyecto1/EstructurasControl.js
@@ -3,17 +3,73 @@ const prompt = require('prompt-sync')(); //Llama a la función nativa de Node pa
 
 function AdivinarNumero(){
 
-    let randomNumber = Math.floor(Math.random() * 101); //Genera un número entero entre 0 y 100
+    let randomNumber = Math.floor(Math.random() * 100); //Genera un número entero entre 0 y 100
     let randomNumberUser = prompt('Ingrese un número: ')//Pide el número al usuario
     console.log(randomNumber);
     if(randomNumber == randomNumberUser){
-        console.log("Acertaste, los números son iguales")
+        console.log("Acertaste, los números son iguales");
     }
     else if(randomNumber > randomNumberUser){
-        console.log("El número que debes adivinar es más alto")
+        console.log("El número que debes adivinar es más alto");
     }
     else {
-        console.log("El número que debes adivinar es más bajo")
+        console.log("El número que debes adivinar es más bajo");
 
     }
 }
+
+function CalculadoraFactorial(){
+    let LimiteFactorial = prompt("Ingrese un número para calcular el factorial: ");//Pide el dato al usuario
+    let factorial = 1; //Va a acumular el valor del factorial
+
+    if(LimiteFactorial == 0){
+        console.log("El factorial de 0 es 1");
+    }
+    else {
+        for(let i = LimiteFactorial; i > 0; i--){
+            factorial = factorial * i;
+        }
+        console.log(`El factorial de ${LimiteFactorial} es: ${factorial}`); 
+    }
+}
+
+
+
+/*Salvedad: La función no evalua si es letra mayuscula o minuscula.
+           No reconoce vocales con tildes.
+*/
+function ContadorVocales(){
+    let frase = prompt("Ingrese la frase: ");
+    let vocalA = 0 //Almacena el total de vocales que encuentre en la frase.
+    let vocalE = 0 //Almacena el total de vocales que encuentre en la frase.
+    let vocalI = 0 //Almacena el total de vocales que encuentre en la frase.
+    let vocalO = 0 //Almacena el total de vocales que encuentre en la frase.
+    let vocalU = 0 //Almacena el total de vocales que encuentre en la frase.
+
+    for(let i = 0; i < frase.length; i++){
+        if(frase[i] == "a"){
+            vocalA += 1
+        }
+        else if(frase[i] == "e"){
+            vocalE += 1
+        }
+        else if(frase[i] == "i"){
+            vocalI += 1
+        }
+        else if(frase[i] == "o"){
+            vocalO += 1
+        }
+        else if(frase[i] == "u"){
+            vocalU += 1
+        }
+
+
+    }
+    console.log(`El total de la vocal a es: ${vocalA}`)
+    console.log(`El total de la vocal e es: ${vocalE}`)
+    console.log(`El total de la vocal i es: ${vocalI}`)
+    console.log(`El total de la vocal o es: ${vocalO}`)
+    console.log(`El total de la vocal u es: ${vocalU}`)
+    
+}
+ContadorVocales()
