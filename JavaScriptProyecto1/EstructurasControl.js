6f824d5b29d300a31441103e01ed2c71830cb3f7
LizethVictoria20 <lizvictoria754@gmail.com>
2024-04-14 00:47:42 -0500

    Calculando factorial con for

--- JavaScriptProyecto1/EstructurasControl.js
+++ JavaScriptProyecto1/EstructurasControl.js
@@ -18,3 +18,20 @@ function AdivinarNumero(){
     }
 }
 
+function CalculadoraFactorial(){
+    let LimiteFactorial = prompt("Ingrese un número para calcular el factorial: ")//Pide el dato al usuario
+    let factorial = 1; //Va a acumular el valor del factorial
+
+    if(LimiteFactorial == 0){
+        console.log("El factorial de 0 es 1")
+    }
+    else {
+        for(let i = LimiteFactorial; i > 0; i--){
+            factorial *= i;
+        }
+        console.log(`El factorial de ${LimiteFactorial} es: ${factorial}`);
+    }
+    
+}
+CalculadoraFactorial()
+
