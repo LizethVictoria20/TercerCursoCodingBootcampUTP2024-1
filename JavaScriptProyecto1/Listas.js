2e9e0c8b41314e68eebce1f0ddf77dd3a7b7ced2
LizethVictoria20 <lizvictoria754@gmail.com>
2024-04-14 15:36:12 -0500

    Funcion inconclusa de numero max y min

---
+++ JavaScriptProyecto1/Listas.js
@@ -0,0 +1,26 @@
+
+function ListaNumeros(){
+    let lista = [2, 3, 7, 14, 4, 4];
+    let max = [0];
+    let min = [0];
+    for(number of lista){
+        // min = min < number ? min : number;
+        // max = max > number ? max : number;
+        console.log(number)
+    }
+    console.log(max, min)
+//     for(let i = 0; i < lista.length; i++){
+//          if(max <= lista[i]) {
+//              max = lista[i];
+//          }
+//     }
+//     console.log(max)
+//     for(let i = 0; i < lista.length; i++){
+//         if(min > lista[i]) {
+//             min = lista[i]
+//         }
+//    }
+//    console.log(min)
+}
+
+ListaNumeros()
