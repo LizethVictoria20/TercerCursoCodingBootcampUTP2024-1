0c87d304db5929d971861eb590a9a5e994578646
LizethVictoria20 <lizvictoria754@gmail.com>
2024-04-13 20:05:47 -0500

    Agregando descripción de variables

--- JavaScriptProyecto1/VariablesTiposDatos.js
+++ JavaScriptProyecto1/VariablesTiposDatos.js
@@ -36,49 +36,49 @@
 
 
 
-const prompt = require('prompt-sync')();
+const prompt = require('prompt-sync')(); //Llama a la función nativa de Node para poder hacer uso del prompt
 
 function Convertion(){
   console.log("Convertir Celsius a Fahrenheit");
-  let  celcius = prompt("Por favor digite los centigrados a convertir a Fahrenheit: ");
-  let fahrenheit = (9 / 5 * celcius) + 32;
+  let  celcius = prompt("Por favor digite los centigrados a convertir a Fahrenheit: "); //Pide valor de celcius al usuario
+  let fahrenheit = (9 / 5 * celcius) + 32; //Guarda el resultado de la operación que hace la conversión de C° a F°
   console.log(`${celcius}C° son ${fahrenheit} grados Fahrenheit`);
 }
 
 function IndiceMasaCorporal(){
   console.log("Calcula el Indice de Masa Corporal");
-  let kilogramos = prompt("Por favor, ingresa tu peso en kilogramos: ");
-  let altura = prompt("Por favor, ingresa tu altura en metros: ");
-  let TotalCalculoMasaCorporal = kilogramos /( altura ** 2);
-  let DecimalesCalculoMasaCorporal = TotalCalculoMasaCorporal.toFixed(2)
+  let kilogramos = prompt("Por favor, ingresa tu peso en kilogramos: "); //Pide al usuario el peso en kilogramos
+  let altura = prompt("Por favor, ingresa tu altura en metros: "); //Pide al usuario la altura en metros
+  let TotalCalculoMasaCorporal = kilogramos /( altura ** 2); //Guarda el resultado de la operación que calcula el IMC
+  let DecimalesCalculoMasaCorporal = TotalCalculoMasaCorporal.toFixed(2) // Guarda el valor de IMC pero solo mostrado con 2 numeros décimales
   console.log(`Tu indice de masa corporal (IMC) es: ${DecimalesCalculoMasaCorporal}`)
 }
 
 function GeneradorNombreUsuario(){
   console.log ("Generardor nombre de usuario");
-  let nombre = prompt("Nombre: ");
-  let apellido = prompt("Apellido: ");
-  let AnioNacimiento = prompt("Año de nacimiento: ");
-  let NombreUsuario = nombre + apellido + AnioNacimiento ;
+  let nombre = prompt("Nombre: "); //Pide al usuario el nombre
+  let apellido = prompt("Apellido: ");//Pide al usuario el apellido
+  let AnioNacimiento = prompt("Año de nacimiento: ");//Pide al usuario el año de nacimiento
+  let NombreUsuario = nombre + apellido + AnioNacimiento ; //Guarda el valor concatenado de nombre, apellido y AnioNacimiento
   console.log(`Nombre de usuario combinado: ${NombreUsuario}`);
 }
 
 function Fibonacci(){
-  let total = prompt('Ingrese el valor de n: ')
-  let firstPos = 0;
-  let secodPos = 1;
-  let suma = 1;
+  let PosicionFibonacci = prompt('Ingrese el valor de n: ')//Pide al usuario la posición que quiere conocer de los números de Fibonacci
+  let FirstPos = 0; //Guarda el primer valor de Fibonacci
+  let SecodPos = 1; //Guarda el segundo valor de Fibonacci
+  let suma = 1;//Guardará el resultado de la suma de FirstPos y SecodPos
 
-  if(total == 1){
+  if(PosicionFibonacci == 1){
     console.log("La primera posición es igual a 0")
   }
   else {
-    for(let i = 2; i < total; i++){
-      suma = firstPos + secodPos;
-      firstPos = secodPos;
-      secodPos = suma;
+    for(let i = 2; i < PosicionFibonacci; i++){
+      suma = FirstPos + SecodPos;//Se sobreescriben los valores iniciales
+      FirstPos = SecodPos; //Se sobreescriben los valores iniciales
+      SecodPos = suma;//Se sobreescriben los valores iniciales
     }
-    console.log(`El ${total}-ésimo número de Fibonacci es ${suma}`);
+    console.log(`El ${PosicionFibonacci}-ésimo número de Fibonacci es ${suma}`);
   }
 }
 
@@ -90,7 +90,7 @@ function Menu(){
   console.log("3. Generar nombre de usuario");
   console.log("4. Generar número Fibonacci");
   console.log("5. Salir");
-  let option = prompt("Seleccione una opción: ");
+  let option = prompt("Seleccione una opción: "); //Pide al usuario la opción que quiere ejecutar del menú.
 
   switch(option) {
     case "1":
