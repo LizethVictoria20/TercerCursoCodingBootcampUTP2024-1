4a7caccfb069406536f44323bfdcc619ec238c94
LizethVictoria20 <lizvictoria754@gmail.com>
2024-04-14 16:28:20 -0500

    Funcion que encuentra número maximo de lista

--- JavaScriptProyecto1/Listas.js
+++ JavaScriptProyecto1/Listas.js
@@ -1,28 +1,17 @@
 
 function ListaNumeros(){
     let lista = [2, 3, 7, 14, 4, 4];
-    let max = [0];
-    let min = [0];
-    for(number of lista){
-        // min = min < number ? min : number;
-        // max = max > number ? max : number;
-        console.log(number)
-    }
-    console.log(max, min)
-//     for(let i = 0; i < lista.length; i++){
-//          if(max <= lista[i]) {
-//              max = lista[i];
-//          }
-//     }
-//     console.log(max)
-//     for(let i = 0; i < lista.length; i++){
-//         if(min > lista[i]) {
-//             min = lista[i]
-//         }
-//    }
-//    console.log(min)
-}
+    let max = 0;
+    let min = 0;
 
+    for(let i = 0; i < lista.length; i++){
+         if(max <= lista[i]) {
+             max = lista[i];
+         }
+    }
+    console.log(max)
+}
+ListaNumeros()
 
 function ConcatenarListar(){
     let lista1 = [4, 9, 2]
@@ -35,8 +24,6 @@ function ConcatenarListar(){
     console.log(OrdenarLista)
 
 }
-ConcatenarListar()
-
 
 function InvertirLista(){
     let lista = [2, 3, 7, 4, 1, 9];
