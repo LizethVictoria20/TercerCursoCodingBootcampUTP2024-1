022babe8b002091bc6c1d94fb2c74100fa08ef3a
LizethVictoria20 <lizvictoria754@gmail.com>
2024-04-16 16:08:46 -0500

    Suma de los valores de Fibonacci

---
+++ JavaScriptProyecto2/ValorSumaFibonacci.js
@@ -0,0 +1,26 @@
+function Fibonacci(){
+    let FirstPos = 0; //Guarda el primer valor de Fibonacci
+    let SecodPos = 1; //Guarda el segundo valor de Fibonacci
+    let suma = 1;//Guardará el resultado de la suma de FirstPos y SecodPos
+    let lista = [];
+    let functionReduce;
+    let sumaTotal = 0
+    for(let i = 1; i < 11; i++){
+        if(FirstPos < 100){
+            suma = FirstPos + SecodPos;//Se sobreescriben los valores iniciales
+            FirstPos = SecodPos; //Se sobreescriben los valores iniciales
+            SecodPos = suma;//Se sobreescriben los valores iniciales
+            lista.push(suma);
+        }
+        else{
+            console.log("Terminó")
+        }
+    }
+    functionReduce = lista.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        sumaTotal,
+      );
+    console.log(lista)
+    console.log(`y su suma es: ${functionReduce + 1}`)
+}
+Fibonacci()
