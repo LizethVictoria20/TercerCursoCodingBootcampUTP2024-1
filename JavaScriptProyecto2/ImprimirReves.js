f74f5e25eaafd8b8c183e92ffb8fea4c00d42593
LizethVictoria20 <lizvictoria754@gmail.com>
2024-04-17 15:27:33 -0500

    Funcion que imprimer un numero al reves

--- JavaScriptProyecto2/ImprimirReves.js
+++ JavaScriptProyecto2/ImprimirReves.js
@@ -2,9 +2,15 @@ const prompt = require('prompt-sync')(); //Llama a la función nativa de Node pa
 
 function ReverseNumber(){
     let numero = prompt("Ingresar número: ");
-    let separar = numero.split("");
-    let invertir = separar.reverse()
-    let unir = invertir.join("")
-    console.log(unir);
+    if(Number(numero)){
+        let separar = numero.split("");
+        let invertir = separar.reverse()
+        let unir = invertir.join("")
+        console.log(unir); 
+}
+    else {
+        console.log("Ingresa un número")
+        ReverseNumber()
+    }
 }
 ReverseNumber()
