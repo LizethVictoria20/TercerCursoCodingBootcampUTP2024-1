601b7412a0a7c29756984283b95d03c04c562f4e
LizethVictoria20 <lizvictoria754@gmail.com>
2024-04-19 11:34:10 -0500

    Update versión

--- JavaScriptProyecto2/3.ImprimirReves.js
+++ JavaScriptProyecto2/3.ImprimirReves.js
@@ -1,7 +1,7 @@
 /*
 - Fecha de publicación: 16.04.2023
 - Hora de publicación: 15:30pm
-- Versión de su código: 31
+- Versión de su código: 64
 - Autores: Ing(c):   Luisa Victoria, Lizeth Victoria, Andrea Zapata, Juan Manuel Hurtado, Santiago Castaño, Andres Jaramillo, Camilo
 - Nombre del lenguaje utilizado: “ECMASCRIPT 6.0” (javascript 6.0)
 - Versión del lenguaje utilizado: 6.0
