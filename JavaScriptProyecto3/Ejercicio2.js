50046d02d95ae3c141a65848ec8cac58f15d7e95
LizethVictoria20 <lizvictoria754@gmail.com>
2024-04-20 14:37:11 -0500

    Matriz y reemplazo de 1 y 0

---
+++ JavaScriptProyecto3/Ejercicio2.js
@@ -0,0 +1,31 @@
+function CodigoQR(){
+    //1 represente un pixel negro
+    //1 represente un pixel blanco
+    let matriz = [];
+    let rows = 30;
+    let columns = 30;
+    let max = 2;
+     
+    //Se crean las filas y columnas
+    for(let i = 0; i < rows; i++){
+        matriz[i] = [] //Agrega las 30 filas
+        for(let j = 0; j < columns; j++){
+            matriz[i][j] = Math.floor(Math.random() * max)
+        }
+    }
+
+    //Reemplaza los valores de 0 y 1 por el codigo ASCII 219 y 32
+    for(let i = 0; i < matriz.length; i++){
+        for(let j = 0; j < matriz[i].length; j++){
+            if(matriz[i][j] == 1){
+                process.stdout.write("█")
+            }
+            else if(matriz[i][j] === 0){
+                process.stdout.write(" ")
+            }
+        }
+    }
+
+    console.log(matriz[0, 29])
+}
+CodigoQR();
