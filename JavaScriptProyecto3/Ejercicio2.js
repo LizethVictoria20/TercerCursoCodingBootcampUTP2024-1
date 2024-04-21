6164606752b81179ec1dea07b4f92800e0061c14
LizethVictoria20 <lizvictoria754@gmail.com>
2024-04-21 00:12:27 -0500

    Generando codigo QR

--- JavaScriptProyecto3/Ejercicio2.js
+++ JavaScriptProyecto3/Ejercicio2.js
@@ -14,18 +14,34 @@ function CodigoQR(){
         }
     }
 
-    //Reemplaza los valores de 0 y 1 por el codigo ASCII 219 y 32
+
+    //Agregando la periferia
     for(let i = 0; i < matriz.length; i++){
         for(let j = 0; j < matriz[i].length; j++){
-            if(matriz[i][j] == 1){
-                process.stdout.write("█")
-            }
-            else if(matriz[i][j] === 0){
-                process.stdout.write(" ")
+            if(i === 0 && i === rows - 1 && j === 0 && columns - 1)
+            {
+                matriz[i][j] = 0;
             }
         }
     }
 
-    console.log(matriz[0, 29])
+    //Reemplaza los valores de 0 y 1 por el codigo ASCII 219 y 32
+    for (let i = 0; i < matriz.length; i++) {
+        for (let j = 0; j < matriz[i].length; j++) {
+            if(matriz[i][j] == 1){
+                 process.stdout.write("█");
+            }
+            else {
+             process.stdout.write(" ");
+            }
+            
+        }
+        process.stdout.write("\n");
+ 
+    }
+
 }
 CodigoQR();
+
+
+
