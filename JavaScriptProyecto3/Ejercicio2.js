39df6ab1e94098afabf2feb48d79acbd17c06f06
LizethVictoria20 <lizvictoria754@gmail.com>
2024-04-21 15:37:41 -0500

    Update

--- JavaScriptProyecto3/Ejercicio2.js
+++ JavaScriptProyecto3/Ejercicio2.js
@@ -45,3 +45,54 @@ function CodigoQR(){
 }
 CodigoQR();
 
+console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+
+// function CodigoQR2(){
+//     //1 represente un pixel negro
+//     //1 represente un pixel blanco
+//     let matriz = [];
+//     let rows = 30;
+//     let columns = 30;
+//     let max = 2;
+
+//     //Se crean las filas y columnas
+//     for(let i = 0; i < rows; i++){
+//         matriz[i] = [] //Agrega las 30 filas
+//         for(let j = 0; j < columns; j++){
+//             matriz[i][j] = Math.floor(Math.random() * max)
+//         }
+//     }
+
+
+//     //Agregando la periferia
+//     for(let i = 0; i < matriz.length; i++){
+//         for(let j = 0; j < matriz[i].length; j++){
+//             if(i === 0 && i === rows - 1 && j === 0 && columns - 1)
+//             {
+//                 matriz[i][j] = 0;
+//             }
+//         }
+//     }
+
+//     //Reemplaza los valores de 0 y 1 por el codigo ASCII 219 y 32
+    
+//     for (let i = 0; i < matriz.length; i++) {
+//         process.stdout.write(i.toString())
+//         process.stdout.write("  ")
+
+//         for (let j = 0; j < matriz[i].length; j++) {
+//             if(matriz[i][j] % 2 == 0){
+//                 process.stdout.write("|");
+//             }
+//             else{
+//                 process.stdout.write("+");
+//             }
+
+           
+//         }
+//         process.stdout.write("\n");
+//     }
+
+// }
+// CodigoQR2()
+
