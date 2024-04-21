2be37e34317262abf5c4ca0ca562eb5649cc767d
LizethVictoria20 <lizvictoria754@gmail.com>
2024-04-21 16:12:50 -0500

    Reemplazando valor del array

--- JavaScriptProyecto3/Ejercicio5.js
+++ JavaScriptProyecto3/Ejercicio5.js
@@ -1,8 +1,13 @@
+const prompt = require('prompt-sync')(); //Llama a la función nativa de Node para poder hacer uso del prompt
+
+
 function Triqui(){
     let matriz = [];
     let rows = 3;
     let columns = 3;
     let position = 1;
+    let jugadaJugador1 = "X"
+    let jugadaJugador2 = "O"
 
     
         for(let i = 0; i < rows; i++){
@@ -22,6 +27,33 @@ function Triqui(){
             // process.stdout.write("+");
             process.stdout.write("\n");
         }
+
+    console.log("Jugador 1")
+    let jugador1 = parseInt(prompt("Ingrese el número de la casilla: "));
+
+    console.log("Jugador 2")
+    let jugador2 = parseInt(prompt("Ingrese el número de la casilla: "));
+
+
+    for(let i = 0; i < matriz.length; i++){
+        for (let j = 0; j < matriz[i].length; j++) {
+            if(jugador1 === matriz[i][j] && jugador2 === matriz[i][j]){
+                console.log("Ingrese una casilla diferente")
+                return Triqui()
+            }
+            if(jugador1 === matriz[i][j]){
+                matriz[i][j] = jugadaJugador1
+                Triqui()
+            }
+            if(jugador2 === matriz[i][j]){
+                matriz[i][j] = jugadaJugador2
+                console.log(matriz)
+                Triqui()
+            } 
+        }
+    }
+    
 }
+
 
 Triqui()
