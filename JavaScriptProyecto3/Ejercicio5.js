b43406c0f027d856f6a8a30c4c434fbd89474acb
LizethVictoria20 <lizvictoria754@gmail.com>
2024-04-22 13:16:57 -0500

    Agragando documentación

--- JavaScriptProyecto3/Ejercicio5.js
+++ JavaScriptProyecto3/Ejercicio5.js
@@ -1,13 +1,39 @@
+/*
+- Fecha de publicación: 20.04.2023
+- Hora de publicación: 15:30pm
+- Versión de su código: 74
+- Autores: Ing(c):  Lizeth Victoria
+- Nombre del lenguaje utilizado: “ECMASCRIPT 6.0” (javascript 6.0)
+- Versión del lenguaje utilizado: 6.0
+- Universidad Tecnológica de Pereira
+- Programa de Ingeniería de Sistemas y Computación
+- Descripción de la funcionalidad del programa: 
+  O y X se  marcan en los espacios de un tablero de 3×3 alternadamente dependiendo 
+  del la posicion que escoja el jugador.
+    Output: 1|2|3|
+            -+-+-+
+            4|5|6|
+            -+-+-+
+            7|8|9|
+            -+-+-+
+        Jugador 1
+        Ingrese el número de la casilla:
+        Jugador 2
+        Ingrese el número de la casilla      
+*/
+
+
+
 const prompt = require('prompt-sync')(); //Llama a la función nativa de Node para poder hacer uso del prompt
 
 
 function Triqui(){
-    let matriz = [];
-    let rows = 3;
-    let columns = 3;
-    let position = 1;
-    let jugadaJugador1 = "X"
-    let jugadaJugador2 = "O"
+    let matriz = []; //Almacena los datos que se crearán 
+    let rows = 3; // Dice cuantas filas se crearán
+    let columns = 3; // Dice cuantas columnas se crearán
+    let position = 1; //Variable que ayudará a llenar los espacios de los arrays
+    let jugadaJugador1 = "X" //Identificador del jugador 1
+    let jugadaJugador2 = "O" //Identificador del jugador 2
 
     
         for(let i = 0; i < rows; i++){
