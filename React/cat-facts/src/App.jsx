23fb6fd073fecf9ddc92790c84fe77c4d260924e
LizethVictoria20 <lizvictoria754@gmail.com>
2024-06-01 00:50:41 -0500

    Versión casi funcional

--- React/cat-facts/src/App.jsx
+++ React/cat-facts/src/App.jsx
@@ -2,17 +2,20 @@ import { UseGatito } from "./useGatito.js";
 import { useFact } from "./useFact.js";
 import { Button } from "./button.jsx";
 
-
 function App() {
-
   const  { gatitos } = UseGatito('https://api.thecatapi.com/v1/images/search');
   const { facts } = useFact('https://catfact.ninja/fact');
+
   return (
     <div className="App">
       <h1>Little cats</h1>
       <div>{ gatitos?.map((gatito) => <img src={gatito.url} key={gatito.id} alt="" />)}</div>
       <p>{facts.fact}</p>
-      <Button  text='Update'/>
+        <div>
+          <button onClick={facts}>
+            Presss
+          </button>
+        </div>
     </div>
   );
 }
--- React/cat-facts/src/button.jsx
+++ React/cat-facts/src/button.jsx
@@ -1,21 +1,4 @@
 import { useEffect, useState } from "react";
-import { UseGatito } from "./useGatito";
+import { useGatito } from "./useGatito";
 
 
-export const Button = (props) => {
-    const { gatito, fetchGatito } = UseGatito();
-
-//     const handleClick = () => {
-//         fetchGatito();
-//     }
-//     useEffect(() => {
-//     fetchGatito(); // Opcional: Llamar fetchGatito al montar el componente para obtener una imagen inicial
-//   }, []);
-
-
-//     return (
-//         <div>
-//             <button onClick={handleClick}>{props.text}</button>
-//         </div>
-//     )
-}
