6ca6fb4ed18ad0b3a4a871357da3cc7175448d30
LizethVictoria20 <lizvictoria754@gmail.com>
2024-06-09 16:32:39 -0500

    Update styles

--- React/to-do-list/src/App.js
+++ React/to-do-list/src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import TaskList from "./components/TaskList.jsx";
-// import Header from "./images/header.png";
+import Header from "./images/header.png";
 
 function App() {
   return (
     <div className="App">
-      {/* <div>
-        <img className='header' src={Header} alt="" />
-      </div> */}
+      <div>
+        <img className="header" src={Header} alt="" />
+      </div>
       <div className="container">
         <h1>To Do List</h1>
         <TaskList />
