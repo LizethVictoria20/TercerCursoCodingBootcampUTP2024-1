6b1f20a51808f8977701242544b1984d15cc40a4
LizethVictoria20 <lizvictoria754@gmail.com>
2024-06-09 16:18:36 -0500

    Marcando como done las tareas

--- React/to-do-list/src/components/TaskList.jsx
+++ React/to-do-list/src/components/TaskList.jsx
@@ -1,9 +1,9 @@
-import FormTask from './FormTask.jsx';
-import Task from './Task.jsx';
+import FormTask from "./FormTask.jsx";
+import Task from "./Task.jsx";
 import { useState } from "react";
 
 function TaskList() {
-  const [tasks, setTask] = useState([])
+  const [tasks, setTask] = useState([]);
 
   const addTask = (task) => {
     if (task.text.trim()) {
@@ -12,28 +12,41 @@ function TaskList() {
       const updateTask = [task, ...tasks];
       setTask(updateTask);
     }
-  }
+  };
 
-  const deleteTask = () => {
+  const deleteTask = (id) => {
+    const updateTask = tasks.filter((task) => task.id !== id);
+    console.log(updateTask);
+    setTask(updateTask);
+  };
 
-  }
+  const completedTask = (id) => {
+    const updateTask = tasks.map((task) => {
+      if (task.id === id) {
+        task.completed = !task.completed;
+      }
+      return task;
+    });
+    setTask(updateTask);
+  };
 
   return (
     <>
-        <FormTask onSubmit = {addTask}/>
-        <div className='container-list-task'>
-          {
-            tasks.map((task) => 
-              <Task 
-              key={task.id} 
-              id={task.id} 
-              text={task.text} 
-              completed={task.completed}
-              /> )
-          }
-        </div>
+      <FormTask onSubmit={addTask} />
+      <div className="container-list-task">
+        {tasks.map((task) => (
+          <Task
+            key={task.id}
+            id={task.id}
+            text={task.text}
+            completed={task.completed}
+            completedTask={completedTask}
+            deleteTask={deleteTask}
+          />
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default TaskList
+export default TaskList;
