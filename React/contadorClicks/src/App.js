1ae8758d6216605529a14ae7e2db939c7fb3db8f
LizethVictoria20 <lizvictoria754@gmail.com>
2024-06-09 22:59:31 -0500

    Agregando botón para disminuir

--- React/contadorClicks/src/App.js
+++ React/contadorClicks/src/App.js
@@ -1,26 +1,32 @@
 import { Button } from "./components/Button.jsx";
 import { Contador } from "./components/Contador.jsx";
-import { useState } from 'react';
+import { useState } from "react";
 
 function App() {
-  const [count, setCounter] = useState(0)
-  
+  const [count, setCounter] = useState(0);
+
   const aumentarContador = () => {
-    setCounter(count + 1)
-  }
+    setCounter(count + 1);
+  };
+
+  const disminirContador = () => {
+    setCounter(count - 1);
+  };
+
   const resetContador = () => {
-    setCounter(0)
-  }
+    setCounter(0);
+  };
 
   return (
     <div className="App">
       <Contador count={count} />
-      <Button text='Click' manejarClick={aumentarContador} queBoton={true}/>
-      <Button text='Reset' manejarClick={resetContador} queBoton={false}/>
+      <div>
+        <Button text="+" manejarClick={aumentarContador} queBoton={true} />
+        <Button text="-" manejarClick={disminirContador} queBoton={true} />
+      </div>
+      <Button text="Reset" manejarClick={resetContador} queBoton={false} />
     </div>
   );
 }
 
 export default App;
-
-
--- React/contadorClicks/src/components/Button.jsx
+++ React/contadorClicks/src/components/Button.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import '../styles/Button.css';
-
+import React from "react";
+import "../styles/Button.css";
 
 export function Button({ text, manejarClick, queBoton }) {
-    return (
-      <div>
-        <button className = {queBoton ? 'Click' : 'Reset'}
-          onClick={manejarClick}>
-          {text}
-        </button>
-      </div>
-    );
+  return (
+    <>
+      <button className={queBoton ? "Click" : "Reset"} onClick={manejarClick}>
+        {text}
+      </button>
+    </>
+  );
 }
