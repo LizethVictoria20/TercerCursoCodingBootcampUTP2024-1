c68113318d0e302add4501968ef3e8ebd0a1b33b
LizethVictoria20 <lizvictoria754@gmail.com>
2024-06-10 12:52:22 -0500

    agregando rutas

--- React/api-jojos/src/components/Cards.jsx
+++ React/api-jojos/src/components/Cards.jsx
@@ -1,37 +1,24 @@
-import { RenderAPI } from './apiFetch.jsx';
-import { Information } from './Information.jsx';
-import '../styles/cards.css';
-import { Route, Routes, Link } from 'react-router-dom';
+import { RenderAPI } from "./apiFetch.jsx";
+import { Information } from "./Information.jsx";
+import "../styles/cards.css";
 
-
-export function Cards({props}){
-  const { character } = RenderAPI('https://stand-by-me.herokuapp.com/api/v1/characters');
-  const urlImg = 'https://jojos-bizarre-api.netlify.app/assets/';
-
-   <Routes>
-        <Route path='*' element={<Cards />} >
-          <Route path='/information' element={<Information/>} />
-        </Route>
-      </Routes>
+export function Cards({ props }) {
+  const { character } = RenderAPI(
+    "https://stand-by-me.herokuapp.com/api/v1/characters"
+  );
+  const urlImg = "https://jojos-bizarre-api.netlify.app/assets/";
 
   return (
-    <div className='container'>
-      {character?.map((data) => 
-      <div key={data.id} className='card'>
-          <h3 className='character-name'>{data.name}</h3>
+    <div className="container">
+      {character?.map((data) => (
+        <div className="card" key={data.id}>
+          <h3 className="character-name" id={data.id}>
+            {data.name}
+          </h3>
           <p>{data.japaneseName}</p>
-          <img src={urlImg + data.image} alt='Imagen personaje'/>
-
-      </div>)}
-      <Routes>
-        <Route path='/' element={<Cards />} >
-          <Route path='/information' element={<Information/>} />
-        </Route>
-      </Routes>
+          <img src={urlImg + data.image} alt="Imagen personaje" />
+        </div>
+      ))}
     </div>
-    
-    
-
-  )
-    
+  );
 }
