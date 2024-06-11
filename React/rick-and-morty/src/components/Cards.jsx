1d8f0aa8bada3c7bd5619a36efc8595a88f7a85a
LizethVictoria20 <lizvictoria754@gmail.com>
2024-06-11 00:11:52 -0500

    Update styles y sitaxis

--- React/rick-and-morty/src/components/Cards.jsx
+++ React/rick-and-morty/src/components/Cards.jsx
@@ -1,19 +1,21 @@
 // Cards.jsx
 import React from "react";
-import '../styles/Card.css'
+import "../styles/Card.css";
 
 const Card = ({ characters, alterEgo, isAlive }) => {
   return (
-    <div className='container'>
-      {characters.map(character => (
-        <div key={character.id} className='container-character'>
+    <div className="container">
+      {characters.map((character) => (
+        <div key={character.id} className="container-character">
           <img src={character.image} alt={character.name} />
-          <img src={alterEgo[character.id]} alt={`${character.name} alter ego`} />
+          <img
+            src={alterEgo[character.id]}
+            alt={`${character.name} alter ego`}
+          />
           <div>
             <h2>{character.name}</h2>
-            <p >{character.species}</p>
+            <p>{character.species}</p>
           </div>
-         
         </div>
       ))}
     </div>
