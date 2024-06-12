3f28dd0100d29e4284c91c504167dc27d666ee28
LizethVictoria20 <lizvictoria754@gmail.com>
2024-06-11 21:00:31 -0500

    Quitando routers

--- React/api-jojos/src/App.jsx
+++ React/api-jojos/src/App.jsx
@@ -1,20 +1,16 @@
 import { Cards } from "./components/Cards.jsx";
 import { Information } from "./components/Information.jsx";
 import Logo from "../src/images/JojosLogo.png";
-import { BrowserRouter as Route, useParams, Router } from "react-router-dom";
 
 function App() {
-  const { id } = useParams();
-  console.log(id);
+  // const { id } = useParams();
+  // console.log(id);
   return (
     <div className="container-app">
       <div className="Logo">
         <img src={Logo} alt="Logo jojos" />
       </div>
       <Cards />
-      <Route path=":id">
-        <Information />
-      </Route>
     </div>
   );
 }
