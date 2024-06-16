02e7ebdbb3431397b569295388c0911fc85f4d44
LizethVictoria20 <lizvictoria754@gmail.com>
2024-06-16 15:55:22 -0500

    Ininit

--- Angular/giphy-gifs/src/app/app.component.ts
+++ Angular/giphy-gifs/src/app/app.component.ts
@@ -1,25 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from './services/api-render.service';
-import { CommonModule } from '@angular/common'
-
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  data: any;
-
-  constructor(private apiService: ApiService) { }
-
-  ngOnInit() {
-    this.apiService.getData().subscribe(response => {
-      this.data = response.data;
-      console.log(this.data)
-      
-    });
-  }
+export class AppComponent {
+  title = 'giphy-gifs';
 }
