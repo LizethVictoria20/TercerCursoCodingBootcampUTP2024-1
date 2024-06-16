f73e81fbae8f2152506e83f58493b14f6b31f937
LizethVictoria20 <lizvictoria754@gmail.com>
2024-06-16 17:19:37 -0500

    Renderizando imagenes en el dom

--- Angular/giphy-gifs/src/app/components/search/search.component.ts
+++ Angular/giphy-gifs/src/app/components/search/search.component.ts
@@ -3,45 +3,47 @@ import { FormControl } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-
-import { map } from 'rxjs';
+import { map, debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
   standalone: true,
   imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.css'
+  styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  searchField = new FormControl('');
+  results: any[] = [];
 
-  searchField = new FormControl
-  results: any[] = []
-
-  constructor(private http: HttpClient) {
-
-  }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.searchField.valueChanges
+    //Lo más lindoooo
+    .pipe(debounceTime(500))
     .subscribe((value) => {
-      this.getData(value)
-    })
+      if (value) {
+        this.getData(value);
+      } else {
+        this.results = [];
+      }
+    });
   }
 
   private getData(query: string) {
-    const api_key = '2aSGCFFTsHv0pK5by4LRUCbv1Ihs9CMl'
-    this.http.get(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${api_key}&limit=2`)
-    .pipe(
-      map((response:any) => {
-        return response.data.map((item) => item.images.original);
-      })
-    )
-    .subscribe((data) => {
-      this.results = data
-      console.log(data)
+    const api_key = '2aSGCFFTsHv0pK5by4LRUCbv1Ihs9CMl';
+    const apiUrl = `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${api_key}&limit=2`;
 
-    })
+    this.http.get(apiUrl)
+      .pipe(
+        map((response: any) => response.data.map((item: any) => item.images.original))
+      )
+      .subscribe(
+        (data) => {
+          this.results = data;
+          console.log(data);
+        }
+      );
   }
 }
-
