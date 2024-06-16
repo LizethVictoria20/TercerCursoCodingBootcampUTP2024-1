db3cd371c0aba64defe13162db390bb5420390d4
LizethVictoria20 <lizvictoria754@gmail.com>
2024-06-16 12:08:36 -0500

    Renderizando API

--- Angular/giphy-gifs/src/app/app.component.ts
+++ Angular/giphy-gifs/src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './services/api-render.service';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: []
 })
-export class AppComponent {
-  title = 'giphy-gifs';
+export class AppComponent implements OnInit {
+  data: any;
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.apiService.getData().subscribe(response => {
+      this.data = response;
+      console.log(response.data.images)
+    });
+  }
 }
--- Angular/giphy-gifs/src/app/services/api-render.service.ts
+++ Angular/giphy-gifs/src/app/services/api-render.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApiRenderService {
+export class ApiService {
 
-  constructor() { }
+  private api_key = '2aSGCFFTsHv0pK5by4LRUCbv1Ihs9CMl';  // Reemplaza con la URL de tu API
+
+  constructor(private http: HttpClient) { }
+
+  getData(): Observable<any> {
+    return this.http.get<any>(`https://api.giphy.com/v1/gifs/random?api_key=${this.api_key}`);
+  }
 }
